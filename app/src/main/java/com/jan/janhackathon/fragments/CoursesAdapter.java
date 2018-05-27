168e70a218b43536c30bd486b4a5ec05fceb3a7f
Jerry Adeleye <mokomiab@gmail.com>
2018-05-27 17:35:44 +0100

    dashboard added

---
+++ app/src/main/java/com/jan/janhackathon/fragments/CoursesAdapter.java
@@ -0,0 +1,63 @@
+package com.jan.janhackathon.fragments;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jan.janhackathon.R;
+import com.jan.janhackathon.custom.ProgressCircleView;
+
+public class CoursesAdapter extends RecyclerView.Adapter<CoursesAdapter.CoursesViewHolder> {
+
+    private final Context mContext;
+
+    public CoursesAdapter(Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public CoursesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_item, parent, false);
+        view.setFocusable(true);
+
+        return new CoursesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CoursesViewHolder holder, int position) {
+
+        holder.courseTitleView.setText(R.string.maths);
+        holder.courseDescView.setText(R.string.math_desc);
+        holder.percentageView.setText(R.string.peercentage);
+        holder.progressCircleView.setValue(20);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    class CoursesViewHolder extends RecyclerView.ViewHolder{
+
+        TextView courseTitleView,
+                courseDescView,
+                percentageView;
+        ProgressCircleView progressCircleView;
+
+        public CoursesViewHolder(View itemView) {
+            super(itemView);
+            courseTitleView = itemView.findViewById(R.id.course_title_text);
+            courseDescView = itemView.findViewById(R.id.course_desc_text);
+            percentageView = itemView.findViewById(R.id.percentage_text);
+            progressCircleView = itemView.findViewById(R.id.percentage_circle_view);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/jan/janhackathon/fragments/DashboardFragment.java
@@ -0,0 +1,59 @@
+package com.jan.janhackathon.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jan.janhackathon.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DashboardFragment extends Fragment {
+
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    public static DashboardFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        DashboardFragment fragment = new DashboardFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private RecyclerView mCoursesRecyclerView;
+    private CoursesAdapter mAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new CoursesAdapter(getContext());
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mCoursesRecyclerView = rootView.findViewById(R.id.courses_recycler_view);
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        mCoursesRecyclerView.setLayoutManager(layoutManager);
+        mCoursesRecyclerView.setHasFixedSize(true);
+        mCoursesRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+
+}
